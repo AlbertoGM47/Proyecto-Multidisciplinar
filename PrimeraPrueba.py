0c4ae78819b0efbbff01f4f7a1df9f84ef04a898
AlbertoGM47 <alberto.gutierrez.morejon@estudiantes.uva.es>
2025-04-06 19:39:09 +0200

    Update PrimeraPrueba.py

--- PrimeraPrueba.py
+++ PrimeraPrueba.py
@@ -8,5 +8,13 @@ button1=st.button("Pedido nuevo")
 if button1:
   st.write("Has pulsado en el boton :)")
 
-like = st.checkbox("Soporte SP1 seleccionado")
+like1 = st.checkbox("Soporte S1 seleccionado")
+like2 = st.checkbox("Rueda WN2 seleccionado")
+like3 = st.checkbox("Casquillo M12 seleccionado")
+like4 = st.checkbox("Rodamiento seleccionado")
+like5 = st.checkbox("Tornillo M8x60 seleccionado")
+like6 = st.checkbox("Tuerca M8 seleccionado")
 
+button2=st.button("Ensamblaje")
+if button2:
+  st.write(like1,like2,like3,like4,like5,like6)
