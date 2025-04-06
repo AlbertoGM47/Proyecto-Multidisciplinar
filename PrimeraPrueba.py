be33d4f98939060cde037606122e24f7a236740c
AlbertoGM47 <alberto.gutierrez.morejon@estudiantes.uva.es>
2025-04-06 19:13:01 +0200

    Update PrimeraPrueba.py

--- PrimeraPrueba.py
+++ PrimeraPrueba.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Puesto de montaje morrocotudo")
+st.title("Puesto de montaje en un entorno lean😎")
 
 st.write("Esta es la app para el puesto de montaje de los ruedines. Aquí aparecen los pedidos,...")
